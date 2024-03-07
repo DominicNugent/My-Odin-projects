53f54aabb65e4b6838d525638c6f2f285626caac
DominicNugent@users.noreply.github.com <DominicNugent@users.noreply.github.com>
2024-03-06 22:41:52 -0600

    Form training example_files
    Completed practice files while learning about forms.

---
+++ Sign-up-form/signUp-script.js
@@ -0,0 +1,8 @@
+const attribution1 = "https://unsplash.com/it/@haliewestphoto?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash"
+const attribution2 = "https://unsplash.com/it/foto/pianta-a-foglia-verde-in-fotografia-ravvicinata-25xggax4bSA?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash"
+
+const href1 = document.querySelector('.source1');
+const href2 = document.querySelector('.source2');
+
+href1.href = attribution1;
+href2.href = attribution2;
